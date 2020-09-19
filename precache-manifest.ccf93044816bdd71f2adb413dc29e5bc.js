4adc7ad0dfc3b02d1631ca1ece4f312c9fae1554
pvthang1609 <pvthang1609@gmail.com>
2020-09-19 16:36:33 +0700

    Updates

---
+++ precache-manifest.ccf93044816bdd71f2adb413dc29e5bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baed1bfe03ea6bbc9ad2976b13c0efa8",
+    "url": "/todos-app-react/index.html"
+  },
+  {
+    "revision": "228a3f270e36fc314d58",
+    "url": "/todos-app-react/static/css/main.0208d68b.chunk.css"
+  },
+  {
+    "revision": "59afe1483569556d7267",
+    "url": "/todos-app-react/static/js/2.f5c65f3e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todos-app-react/static/js/2.f5c65f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228a3f270e36fc314d58",
+    "url": "/todos-app-react/static/js/main.196cdd95.chunk.js"
+  },
+  {
+    "revision": "c39337cdc90d26a97d0e",
+    "url": "/todos-app-react/static/js/runtime-main.53703320.js"
+  },
+  {
+    "revision": "918ca7d7f7e79feed9272cc0bb8d32f6",
+    "url": "/todos-app-react/static/media/Clear.918ca7d7.svg"
+  },
+  {
+    "revision": "57a87fdb626b8fcb8ece7200e5c6ff57",
+    "url": "/todos-app-react/static/media/Tick-all.57a87fdb.svg"
+  },
+  {
+    "revision": "3a8a773ef20a71fef556741e44be5c6f",
+    "url": "/todos-app-react/static/media/Tick.3a8a773e.svg"
+  },
+  {
+    "revision": "5d1227310dec5d3b4ecffe3b6b798b69",
+    "url": "/todos-app-react/static/media/Un-Tick.5d122731.svg"
+  }
+]);
